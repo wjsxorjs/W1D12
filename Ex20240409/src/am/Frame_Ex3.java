cc7da1f2a2bd78153b16e3eefcf918133c6a39eb
wjsxorjs <wjsxorjs@naver.com>
2024-04-09 12:43:58 +0900

    JAVA 2024.04.09 0.4

--- Ex20240409/src/am/Frame_Ex3.java
+++ Ex20240409/src/am/Frame_Ex3.java
@@ -3,6 +3,8 @@ package am;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
@@ -15,6 +17,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
+import javax.swing.event.MouseInputAdapter;
 
 public class Frame_Ex3 extends JFrame {
 	
@@ -37,7 +40,7 @@ public class Frame_Ex3 extends JFrame {
 		//JPanel이 원해는 FlowLayout이었지만 BorderLayout으로 변경함
 		
 		north_p.add(new JLabel("URL: "),BorderLayout.WEST);
-		north_p.add(url_tf = new JTextField());
+		north_p.add(url_tf = new JTextField("C:/My_Study"));
 		north_p.add(btn = new JButton("확인"),BorderLayout.EAST);
 		
 		this.add(north_p,BorderLayout.NORTH);
@@ -58,34 +61,45 @@ public class Frame_Ex3 extends JFrame {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
-				String path = url_tf.getText().trim();
-				
-				if(path.length()==0) {
-					// 경고창(메세지 창) 보여주기
-					JOptionPane.showMessageDialog(Frame_Ex3.this, "입력된 값이 없습니다!");
-				} else {
-					File f = new File(path);
-					if(f.exists()) {
-						if(f.isDirectory()) {
-							//하위 목록을 얻어낸다.
-							String[] sub = f.list();
-							
-							//얻어낸 배열을 통해 JList 생성
-							center_list.setListData(sub);
-							center_list.revalidate();
-							
-							
-						} else {
-							// 경고창(메세지 창) 보여주기
-							JOptionPane.showMessageDialog(Frame_Ex3.this, "디렉토리가 아닌\n파일입니다!");
-						}
-					} else {
-					// 경고창(메세지 창) 보여주기
-					JOptionPane.showMessageDialog(Frame_Ex3.this, "존재하지 않습니다!");
-						
-					}
+				checkUrl();
+			}
+		});
+		
+		url_tf.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				checkUrl();
+			}
+		});
+		
+		center_list.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// 마우스를 클릭한 count값을 얻어낸다.
+				int cnt = e.getClickCount();
+				if(cnt==2) {
+					// 더블클릭했을때만 수행하는 곳
+					// 선택된 항목을 얻어낸다.
+					String selVal = center_list.getSelectedValue();
+					
+					// 얻어낸 값을 url_tf에 붙여넣어준다.
+					String newPath = new StringBuffer(url_tf.getText())
+																	.append("/")
+																	.append(selVal)
+																	.toString();
+					
+					String p_str =url_tf.getText();
+					char ch = p_str.charAt(p_str.length()-1);
+					StringBuffer newPath2 = new StringBuffer(url_tf.getText());
+					if(ch != '/') newPath2.append("/");
+					newPath2.append(selVal);
+					newPath = newPath2.toString();
+					
+					url_tf.setText(newPath);
+					checkUrl();
 				}
+				
 				
 			}
 		});
@@ -93,6 +107,39 @@ public class Frame_Ex3 extends JFrame {
 		
 	}
 	
+	private void checkUrl() {
+		String path = url_tf.getText().trim();
+		
+		if(path.length()==0) {
+			// 경고창(메세지 창) 보여주기
+			JOptionPane.showMessageDialog(Frame_Ex3.this, "입력된 값이 없습니다!");
+		} else {
+			getFileList(path);
+		}
+
+	}
+	
+	private void getFileList(String path) {
+		File f = new File(path);
+		if(f.exists()) {
+			if(f.isDirectory()) {
+				//하위 목록을 얻어낸다.
+				String[] sub = f.list();
+				
+				//얻어낸 배열을 통해 JList 생성
+				center_list.setListData(sub);
+				
+				
+			} else {
+				// 경고창(메세지 창) 보여주기
+				JOptionPane.showMessageDialog(Frame_Ex3.this, "디렉토리가 아닌\n파일입니다!");
+			}
+		} else {
+		// 경고창(메세지 창) 보여주기
+		JOptionPane.showMessageDialog(Frame_Ex3.this, "존재하지 않습니다!");
+			
+		}
+	}
 
 	public static void main(String[] args) {
 		new Frame_Ex3();
